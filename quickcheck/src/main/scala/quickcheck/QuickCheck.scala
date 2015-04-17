d21180f2a448fff3502ae5eb922c3638fefe549a
Enrico Benini <e.benini@apexnet.it>
2015-04-17 09:40:32 +0200

    Add the heap generator to the check class

--- quickcheck/src/main/scala/quickcheck/QuickCheck.scala
+++ quickcheck/src/main/scala/quickcheck/QuickCheck.scala
@@ -14,7 +14,15 @@ abstract class QuickCheckHeap extends Properties("Heap") with IntHeap {
     findMin(h) == a
   }
 
-  lazy val genHeap: Gen[H] = ???
+  property("gen1") = forAll { (h: H) =>
+    val m = if (isEmpty(h)) 0 else findMin(h)
+    findMin(insert(m, h))==m
+  }
+  
+  lazy val genHeap: Gen[H] = for {
+    k <- arbitrary[Int]
+    m <- oneOf(const(empty), genHeap)
+  } yield insert(k,m)
 
   implicit lazy val arbHeap: Arbitrary[H] = Arbitrary(genHeap)
 
